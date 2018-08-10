c5fe04b2c955b1b906ad8b239b20681f9b7ed849
bttb66 <bttb66@naver.com>
2018-08-10 15:21:08 +0900

    todo-list : optimizing

--- todo-list/src/components/TodoItem.js
+++ todo-list/src/components/TodoItem.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 import './TodoItem.css';
 
 class TodoItem extends Component{
+    
+    //아이템 변경시 배열 전체 리렌더링 되는 것 방지 (변경된 아이템만 리렌더링)
+    shouldComponentUpdate(nextProps, nextStatus){
+        return this.props.checked !== nextProps.checked;
+    }
+
     render(){
         const { text, checked, id, onToggle, onRemove } = this.props;
-
+        console.log(id);
         return (
             <div className="todo-item" onClick={() => onToggle(id)}>
                 <div className="remove" onClick={(e) => {
--- todo-list/src/components/TodoItemList.js
+++ todo-list/src/components/TodoItemList.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import TodoItem from './TodoItem';
 
 class TodoItemList extends Component{
+
+    //input 값 입력될때마다 virtualDOM 리렌더링되는 것 방지
+    shouldComponentUpdate(nextProps, nextState){
+        return this.props.todos !== nextProps.todos;
+    }
+
     render(){
         const { todos, onToggle, onRemove } = this.props;
 
